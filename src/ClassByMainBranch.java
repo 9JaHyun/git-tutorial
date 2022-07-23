601910def426b0d9ebd549bba33c184c4812426c
9JaHyun <jahyunnine@gmail.com>
2022-07-23 10:19:42 +0900

    second modified in main branch

--- src/ClassByMainBranch.java
+++ src/ClassByMainBranch.java
@@ -1,6 +1,6 @@
 public class ClassByMainBranch {
 
     public static void main(String[] args) {
-        System.out.println("Main Branch 에서 한번 수정한 Class");
+        System.out.println("Main Branch 에서 두번 수정한 Class");
     }
 }
