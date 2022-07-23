763d1ffa32824ba6741bef39b330424ccaec21d9
9JaHyun <jahyunnine@gmail.com>
2022-07-23 10:53:05 +0900

    a 수정

--- src/Class_By_B_Branch.java
+++ src/Class_By_B_Branch.java
@@ -1,7 +1,7 @@
 public class Class_By_B_Branch {
 
     public static void main(String[] args) {
-        System.out.println("D Branch 에서 수정한 Class");
+        System.out.println("sdffsfsfdsfdsf");
     }
 
 }
