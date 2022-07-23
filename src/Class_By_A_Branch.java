b4f98e159c7d5f355a84d59888fcdb227cdc9625
9JaHyun <jahyunnine@gmail.com>
2022-07-23 10:46:36 +0900

    ㅁ

--- src/Class_By_A_Branch.java
+++ src/Class_By_A_Branch.java
@@ -1,6 +1,6 @@
 public class Class_By_A_Branch {
 
     public static void main(String[] args) {
-        System.out.println("d branch 에서 수정한 Class");
+        System.out.println("erorr branch에서 수정!!");
     }
 }
