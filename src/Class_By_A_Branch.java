4f1bad1269bb0fc4c0dfe87aa18123122c7eb0da
9JaHyun <jahyunnine@gmail.com>
2022-07-23 10:23:10 +0900

    first modified in d branch

--- src/Class_By_A_Branch.java
+++ src/Class_By_A_Branch.java
@@ -1,6 +1,6 @@
 public class Class_By_A_Branch {
 
     public static void main(String[] args) {
-        System.out.println("A Branch 에서 만든 Class");
+        System.out.println("d branch 에서 수정한 Class");
     }
 }
